9b7a6ff4578b3de8b3b2e908a8fa97c5a5683a6a
James Percival <j.percival@imperial.ac.uk>
2018-09-10 11:48:00 +0100

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quaternions_acse',  # Required
+    version='1.0.0',  # Required
+    packages=['quaternions_acse'],  # Required
+    # One-line description or tagline of what your project does
+    description='A sample implementation of quaternions.',  # Optional
+    url='https://github.com/jrper/quaternions_acse',  # Optional
+    author='James Percival',  # Optional
+    author_email='j.percival@imperial.ac.uk',  # Optional
+)
